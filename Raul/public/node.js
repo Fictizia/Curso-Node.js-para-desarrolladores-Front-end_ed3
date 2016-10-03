c66cab86601b173e9281b25b798c39fb20f5fc05
Raul <huetez@gmail.com>
2016-10-03 19:31:26 +0000

    ejemplo

---
+++ Raul/public/node.js
@@ -0,0 +1,41 @@
+var http = require('http'),
+      url = require('url');
+
+  http.createServer(function (req, res) {
+    var pathname = url.parse(req.url).pathname;
+
+    if (pathname === '/') {
+        res.writeHead(200, {
+        'Content-Type': 'text/plain'
+      });
+      res.end('Index!');
+
+    } else if (pathname === '/¿Quienes somos?') {
+        res.writeHead(200, {
+        'Content-Type': 'text/plain; charset=utf-8'
+      });
+      res.end('sencillamente otra página');
+
+
+    } else if (pathname === '/¿Donde Estamos?') {
+        res.writeHead(200, {
+        'Location': '/'
+      });
+      res.end();    
+
+    } else if (pathname === '/¿Que hacemos?') {
+        res.writeHead(200, {
+        'Location': '/'
+      });
+      res.end();    
+
+    } else {
+        res.writeHead(404, {
+        'Content-Type': 'text/plain'
+      });
+      res.end('Querido... 404!');
+    }
+
+  }).listen(process.env.15454, process.env.IP);
+
+  console.log('Servidor funcionando en http://'+process.env.IP+':'+process.env.15454+'/');
