df5712f35e20eca41572ab7382ad86c64ca4c7e8
Raul <huetez@gmail.com>
2016-10-05 19:43:41 +0000

    terremote

---
+++ Raul/public/terremote.js
@@ -0,0 +1,19 @@
+var http = require('http');
+
+var options = {
+  host: 'earthquake.usgs.gov',
+  path: '/earthquakes/feed/v1.0/summary/significant_hour.geojson'
+};
+
+
+http.request(options, function(response) {
+  var str = '';
+
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  response.on('end', function () {
+    console.log(str);
+  });
+});
